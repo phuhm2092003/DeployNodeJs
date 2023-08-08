c8fddb228fd4b12a7afc5a48b3f1fd638132d5cd
DELL <hominphuzxc@gmail.com>
2023-08-08 08:05:21 +0700

    #Deploy node js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 app.get('/users', (req, res) => {
-    res.send('Get use permission')
+    res.send('Get use permission - render.com')
 })
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
