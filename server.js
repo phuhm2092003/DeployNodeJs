699aa179ed801ba929fe60ccc322c41de71e6972
DELL <hominphuzxc@gmail.com>
2023-08-08 08:01:02 +0700

    #Clean 2

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 app.get('/users', (req, res) => {
-    res.send('Get list user')
+    res.send('Get use permission')
 })
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
